7f3ca5ea9d73d4d02fa9d459ba195c99f5f70cac
Kxffie <bryceriordan2005@gmail.com>
2022-01-14 21:41:21 -0600

    init

--- pages/modules/Navbar/Navbar.tsx
+++ pages/modules/Navbar/Navbar.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import Link from 'next/link'
-import {AppBar, Toolbar, Typography, Box, Button, Container } from '@material-ui/core'
+import {AppBar, Toolbar, Typography, Box, Button, Container, makeStyles } from '@material-ui/core'
 import { YoutubeFill, DiscordFill, GithubFill } from 'akar-icons';
 
+const useStyles = makeStyles({
+    button: {
+        backgroundColor: 'transparent',
+        filter: 'brightness(1.20)',
+        '&:hover': {
+            backgroundColor: '#f6ff9a'
+        },
+        '&:active': {
+            backgroundColor: '#f1ff67'
+        }
+    },
+  });
+  
+
 const Navbar = () => {
+    const classes = useStyles();
+
     return (
         <Box style={{flexGrow: 1}}>
             <AppBar color="inherit">
@@ -15,22 +31,22 @@ const Navbar = () => {
 
                         <Typography variant="h6" color="inherit" component="div" style={{flexGrow: 1}}>
                             <Link href="/">
-                                <Button>home</Button>
+                                <Button className={classes.button}>home</Button>
                             </Link>
                             <Link href="/projects">
-                                <Button>projects</Button>
+                                <Button className={classes.button}>projects</Button>
                             </Link>
                         </Typography>
 
                         <Box component="div" style={{flexGrow: 0}}>
                             <Link href="https://youtube.kxffiehub.xyz/">
-                                <a target="_blank"><Button><YoutubeFill color="black" size={24} /></Button></a>
+                                <a target="_blank"><Button className={classes.button}><YoutubeFill color="black" size={24} /></Button></a>
                             </Link>
                             <Link href="https://discord.kxffiehub.xyz/">
-                                <a target="_blank"><Button><DiscordFill color="black" size={24} /></Button></a>
+                                <a target="_blank"><Button className={classes.button}><DiscordFill color="black" size={24} /></Button></a>
                             </Link>
                             <Link href="https://github.kxffiehub.xyz/">
-                                <a target="_blank"><Button><GithubFill color="black" size={24} /></Button></a>
+                                <a target="_blank"><Button className={classes.button}><GithubFill color="black" size={24} /></Button></a>
                             </Link>
                         </Box>
                     </Toolbar>

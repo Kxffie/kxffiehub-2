01888d670ac0ead0fe6eef4215f9696742a32d4f
Kxffie <bryceriordan2005@gmail.com>
2022-01-14 21:07:03 -0600

    init

--- pages/modules/Navbar/Navbar.tsx
+++ pages/modules/Navbar/Navbar.tsx
@@ -1,26 +1,40 @@
 import React from 'react'
 import Link from 'next/link'
-import {AppBar, Toolbar, Typography, Box, Button } from '@material-ui/core'
+import {AppBar, Toolbar, Typography, Box, Button, Container } from '@material-ui/core'
 import { YoutubeFill, DiscordFill, GithubFill } from 'akar-icons';
 
 const Navbar = () => {
     return (
         <Box style={{flexGrow: 1}}>
             <AppBar color="inherit">
-                <Toolbar>
-                    <Typography variant="h6" color="inherit" component="div" style={{flexGrow: 1}}>
-                        Kxffie Hub
-                    </Typography>
-                    <Link href="https://youtube.kxffiehub.xyz/">
-                    <a target="_blank"><Button><YoutubeFill color="black" size={24} /></Button></a>
-                    </Link>
-                    <Link href="https://discord.kxffiehub.xyz/">
-                    <a target="_blank"><Button><DiscordFill color="black" size={24} /></Button></a>
-                    </Link>
-                    <Link href="https://github.kxffiehub.xyz/">
-                        <a target="_blank"><Button><GithubFill color="black" size={24} /></Button></a>
-                    </Link>
-                </Toolbar>
+                <Container maxWidth="lg">
+                    <Toolbar>
+                        <Typography variant="h6" color="inherit" component="div" style={{flexGrow: 1}}>
+                            Kxffie Hub
+                        </Typography>
+
+                        <Typography variant="h6" color="inherit" component="div" style={{flexGrow: 1}}>
+                            <Link href="/">
+                                <Button>home</Button>
+                            </Link>
+                            <Link href="/projects">
+                                <Button>projects</Button>
+                            </Link>
+                        </Typography>
+
+                        <Box component="div" style={{flexGrow: 0}}>
+                            <Link href="https://youtube.kxffiehub.xyz/">
+                                <a target="_blank"><Button><YoutubeFill color="black" size={24} /></Button></a>
+                            </Link>
+                            <Link href="https://discord.kxffiehub.xyz/">
+                                <a target="_blank"><Button><DiscordFill color="black" size={24} /></Button></a>
+                            </Link>
+                            <Link href="https://github.kxffiehub.xyz/">
+                                <a target="_blank"><Button><GithubFill color="black" size={24} /></Button></a>
+                            </Link>
+                        </Box>
+                    </Toolbar>
+                </Container>
             </AppBar>
         </Box>
     )

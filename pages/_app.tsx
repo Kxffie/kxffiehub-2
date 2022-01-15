3e717e38218ba4a02924e8b052a586d0278fcc60
Kxffie <bryceriordan2005@gmail.com>
2022-01-14 21:23:48 -0600

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,21 @@
+// Import thing
 import Head from 'next/head'
+import Router from 'next/router';
+import nProgress from 'nprogress';
 
+// Styles
 import Layout from "./modules/Layout"
-import styles from '../styles/Layout.module.scss'
+import '../styles/nprogress.css'
 
+// nprogress
+Router.events.on('routeChangeStart', nProgress.start);
+Router.events.on('routeChangeError', nProgress.done);
+Router.events.on('routeChangeComplete', nProgress.done);
+
+// app
 function MyApp({ Component, pageProps }) {
   return (
-    <div className={styles.main}>
+    <div>
       <Head>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400, 500,700&display=swap" />
         <link rel="icon" href="pfp_plain.png" />
